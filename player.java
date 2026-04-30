706a56d6d974437acecc664d1658bc9b11d9fb17
Niyati Thondapu <niyatireddy75@gmail.com>
2026-04-30 11:43:23 -0400

    push

---
+++ player.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class player here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class player
+{
+    //instance variable
+    String person;
+    
+    
+}
